5789ffbdf32faa3ef14e0028cd748753ca9a26a7
Merrick Mount <339474587@GoTVDSB.ca>
2024-04-04 13:14:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using dieclass;
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double userBalance, userBet, userGuess, dieTotal;
+            userBalance = 100;
+            Console.WriteLine("Welcome to the dice roller game");
+            Console.WriteLine($"You have a balance of {userBalance.ToString("C")}");
+            Console.WriteLine("Please Insert your bet amount");
+            userBet = double.Parse( Console.ReadLine() );
+            Console.WriteLine("What number are you betting on?");
+            userGuess = double.Parse( Console.ReadLine() );
+            die die1 = new die();
+            die die2 = new die(4);
+            Console.WriteLine("Rolling");
+            Thread.Sleep(500);
+            die1.RollDie();
+            Console.WriteLine($"Die 1 = {die1}");
+            Console.WriteLine("Rolling");
+            Thread.Sleep(500);
+            die1.DrawRoll();
+            Console.WriteLine($"Die 2 = {die2}");
+            die2.DrawRoll();
+            Console.WriteLine($"The Total is {die1}");
+        }
+    }
+}
---
+++ die.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dieclass
+{
+    public class die
+    {
+
+        private Random _generator;
+        private int _roll;
+
+        public die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public die(int roll)
+        {
+            _generator = new Random();
+            if (roll < 1)
+                _roll = 1;
+            else if (roll > 6)
+                _roll = 6;
+            else
+                _roll = roll;
+        }
+
+        public int Roll { get { return _roll; } }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void DrawRoll()
+        {
+            switch (_roll)
+            {
+                case 1:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine(" ---");
+                    break;
+                case 2:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o  |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|  o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 3:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o  |");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|  o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 4:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 5:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+                    break;
+                case 6:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+            }
+        }
+    }
+}
